f867a006871ed62e7c0982c2a4ef0d11f5273725
Phuc Tran <phuctran93@hotmail.com>
2015-05-29 20:10:02 +0700

    Consignment Management CoreFlow

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/servlet/ConsignmentServlet.java
@@ -0,0 +1,81 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author ACER
+ */
+@WebServlet(name = "ConsignmentServlet",
+        urlPatterns = {
+            "/consignment",
+            "/consignor"})
+public class ConsignmentServlet extends HttpServlet {
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String userPath = request.getServletPath();
+        if (userPath.equals("/consignment")) {
+            String consignmentId = request.getParameter("id");
+            if (consignmentId != null) {
+                userPath = "/consignment_detail";
+            } else if (request.getParameter("search") != null) {
+                userPath = "/consignment_search";
+            }
+        } else if (userPath.equals("/consignor")) {
+
+        }
+
+        String url = "WEB-INF/jsp/view/store" + userPath + ".jsp";
+        try {
+            request.getRequestDispatcher(url).forward(request, response);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
