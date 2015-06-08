032ab9d47fa05db9cc4eb5fcd59976e3a7faf76d
Phuc Tran <phuctran93@hotmail.com>
2015-06-08 09:55:29 +0700

    Remove Demo code

--- Source/Source-Code/HangingProductStore/src/java/hps/dao/ConsignmentDAO.java
+++ Source/Source-Code/HangingProductStore/src/java/hps/dao/ConsignmentDAO.java
@@ -1,7 +1,6 @@
 package hps.dao;
 
-import hps.dto.Consignment;
-import hps.dto.ConsignmentStatus;
+import hps.dto.ConsignmentDTO;
 import java.util.List;
 
 /**
@@ -9,12 +8,9 @@ import java.util.List;
  * @author Phuc Tran
  */
 public interface ConsignmentDAO {
-    void save(Consignment consignment);
-    Consignment get(long id);
+    void save(ConsignmentDTO consignment);
+    ConsignmentDTO get(long id);
     void delete(long id);
-    int getNonViewRequest(long storeId);
-    List<Consignment> getConsigmentsByStore(long storeId);
-    List<Consignment> getConsigmentsByCustomer(long customerId);
-    List<Consignment> getConsignmentByStatus(long storeId, ConsignmentStatus status);
-    void makeConsignmentAsStatus(long id, ConsignmentStatus status);
+    List<ConsignmentDTO> getConsigmentsByStore(long storeId);
+    List<ConsignmentDTO> getConsigmentsByCustomer(long customerId);
 }
