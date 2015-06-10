595bad176ade7489ef4d41e8c4685da09909e3c4
tiendan3108 <tiendan3108@users.noreply.github.com>
2015-06-10 18:39:34 +0700

    update file GlobalVariables.java

--- Source/Source-Code/HangingProductStore/src/java/hps/ultils/GlobalVariables.java
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/GlobalVariables.java
@@ -15,16 +15,6 @@ public class GlobalVariables {
     public static final String AWS_SECRET_KEY = "J3qeXy1QQHJlwCIX4CpXCeuWP3vN6rs1CpSezubJ";// secret key from amazon, pair with access key
     public static final String ENDPOINT = "ecs.amazonaws.com";// location
     public static final String ASS_TAG = "danqt-20";// account given by amazon so we can send request.
-    public static final int NOT_AVAILABLE = 0;// consignor gives information about product but store owner not yet take it.
-    public static final int ORDERED = 1;// some1 order this product but not yet buy it.
-    public static final int SOLD = 2;// product is sold but not yet pay consignor.
-    public static final int ON_WEB = 3;// product is on web and available for search function.
-    public static final int OWNED = 4;// product is out of contract date and out of take back period. Now store owner own it.
-    public static final int OUT_OF_DATE = 5;// product is out of contract date and still in take back period.
-    public static final int CANCEL = 6;// consignor request cancel consignment and not yet take product back.
-    public static final int RETURNED = 7;// product is returned to consignor.
-    public static final int COMPLETED = 8;// product is sold and store owner pay consignor.
-    public static final int CANCEL_ON_WEB = 9; //consignor request cancel consignment while product is on web
     
     public static final String MANAGERMENT_PAGE = "manageProductStatusPage.jsp";
     public static final String PAYMENT_PAGE = "paymentPage.jsp";
