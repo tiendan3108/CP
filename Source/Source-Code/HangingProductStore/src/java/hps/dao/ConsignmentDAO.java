c4bbd628de6588cee40512773b7427607eb85bee
Phuc Tran <phuctran93@hotmail.com>
2015-06-10 09:52:11 +0700

    Update Manage Consignment use case source code.

--- Source/Source-Code/HangingProductStore/src/java/hps/dao/ConsignmentDAO.java
+++ Source/Source-Code/HangingProductStore/src/java/hps/dao/ConsignmentDAO.java
@@ -32,48 +32,7 @@ public class ConsignmentDAO {
 
             rs = stm.executeQuery();
             while (rs.next()) {
-
-                String consignmentID = rs.getString("ConsignmentID");
-                int productID = rs.getInt("ProductID");
-                Integer memberID = rs.getInt("MemberID");
-                int storeOwnerID = rs.getInt("StoreOwnerID");
-                String fullName = rs.getString("FullName");
-                String address = rs.getString("Address");
-                String phone = rs.getString("Phone");
-                String email = rs.getString("Email");
-                String cardNumber = rs.getString("CardNumber");
-                String cardOwner = rs.getString("CardOwner");
-                String fromDate = rs.getString("FromDate");
-                String toDate = rs.getString("ToDate");
-                String raiseWebDate = rs.getString("RaiseWebDate");
-                String status = rs.getString("Status");
-                int period = rs.getInt("Period");
-                float maxPrice = rs.getFloat("MaxPrice");
-                float returnPrice = rs.getFloat("ReturnedPrice");
-                String receiveDate = rs.getString("ReceivedDate");
-
-                result = new ConsignmentDTO();
-                result.setConsigmentID(consignmentID);
-                result.setProductID(productID);
-                if (memberID != null) {
-                    result.setMemberID(memberID);
-                }
-                result.setStoreOwnerID(storeOwnerID);
-                result.setName(fullName);
-                result.setAddress(address);
-                result.setPhone(phone);
-                result.setEmail(email);
-                result.setCardNumber(cardNumber);
-                result.setCardOwner(cardOwner);
-                result.setFromDate(fromDate);
-                result.setToDate(toDate);
-                result.setRaiseWebDate(raiseWebDate);
-                result.setStatus(status);
-                result.setPeriod(period);
-                result.setMaxPrice(maxPrice);
-                result.setReturnPrice(returnPrice);
-                result.setReceivedDate(receiveDate);
-
+                result = getConsignment(rs);
                 populateProduct(result);
             }
         } catch (SQLException ex) {
@@ -111,50 +70,49 @@ public class ConsignmentDAO {
             rs = stm.executeQuery();
             result = new ArrayList<>();
             while (rs.next()) {
-
-                String consignmentID = rs.getString("ConsignmentID");
-                int productID = rs.getInt("ProductID");
-                Integer memberID = rs.getInt("MemberID");
-                int storeOwnerID = rs.getInt("StoreOwnerID");
-                String fullName = rs.getString("FullName");
-                String address = rs.getString("Address");
-                String phone = rs.getString("Phone");
-                String email = rs.getString("Email");
-                String cardNumber = rs.getString("CardNumber");
-                String cardOwner = rs.getString("CardOwner");
-                String fromDate = rs.getString("FromDate");
-                String toDate = rs.getString("ToDate");
-                String raiseWebDate = rs.getString("RaiseWebDate");
-                String status = rs.getString("Status");
-                int period = rs.getInt("Period");
-                float maxPrice = rs.getFloat("MaxPrice");
-                float returnPrice = rs.getFloat("ReturnedPrice");
-                String receiveDate = rs.getString("ReceivedDate");
-
-                ConsignmentDTO consignment = new ConsignmentDTO();
-                consignment.setConsigmentID(consignmentID);
-                consignment.setProductID(productID);
-                if (memberID != null) {
-                    consignment.setMemberID(memberID);
-                }
-                consignment.setStoreOwnerID(storeOwnerID);
-                consignment.setName(fullName);
-                consignment.setAddress(address);
-                consignment.setPhone(phone);
-                consignment.setEmail(email);
-                consignment.setCardNumber(cardNumber);
-                consignment.setCardOwner(cardOwner);
-                consignment.setFromDate(fromDate);
-                consignment.setToDate(toDate);
-                consignment.setRaiseWebDate(raiseWebDate);
-                consignment.setStatus(status);
-                consignment.setPeriod(period);
-                consignment.setMaxPrice(maxPrice);
-                consignment.setReturnPrice(returnPrice);
-                consignment.setReceivedDate(receiveDate);
-
+                ConsignmentDTO consignment = getConsignment(rs);
                 populateProduct(consignment);
+                result.add(consignment);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(ConsignmentDAO.class.getName()).log(Level.SEVERE, null, ex);
+        } finally {
+            try {
+                if (rs != null) {
+                    rs.close();
+                }
+                if (stm != null) {
+                    stm.close();
+                }
+                if (con != null) {
+                    con.close();
+                }
+            } catch (SQLException ex) {
+                Logger.getLogger(ConsignmentDAO.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        }
+        return result;
+    }
+    
+    public List<ConsignmentDTO> findConsignmentByProductName(int storeOwnerId, String productName) {
+        Connection con = null;
+        PreparedStatement stm = null;
+        ResultSet rs = null;
+        List<ConsignmentDTO> result = null;
+        try {
+            con = DBUltilities.makeConnection();
+            String sql = "SELECT *" +
+                         " FROM Consignment AS C JOIN Product AS P ON C.ProductID = P.ProductID" +
+                         " WHERE C.StoreOwnerID = ? AND P.ProductName LIKE ?";
+            stm = con.prepareStatement(sql);
+            stm.setInt(1, storeOwnerId);
+            stm.setString(2, "%" + productName + "%");
 
+            rs = stm.executeQuery();
+            result = new ArrayList<>();
+            while (rs.next()) {
+                ConsignmentDTO consignment = getConsignment(rs);
+                populateProduct(consignment);
                 result.add(consignment);
             }
         } catch (SQLException ex) {
@@ -182,5 +140,50 @@ public class ConsignmentDAO {
 
         ProductDTO product = dao.getProductByID(consignment.getProductID());
         consignment.setProduct(product);
+    }
+
+    private ConsignmentDTO getConsignment(ResultSet rs) throws SQLException {
+        String consignmentID = rs.getString("ConsignmentID");
+        int productID = rs.getInt("ProductID");
+        Integer memberID = rs.getInt("MemberID");
+        int storeOwnerID = rs.getInt("StoreOwnerID");
+        String fullName = rs.getString("FullName");
+        String address = rs.getString("Address");
+        String phone = rs.getString("Phone");
+        String email = rs.getString("Email");
+        String cardNumber = rs.getString("CardNumber");
+        String cardOwner = rs.getString("CardOwner");
+        String fromDate = rs.getString("FromDate");
+        String toDate = rs.getString("ToDate");
+        String raiseWebDate = rs.getString("RaiseWebDate");
+        String status = rs.getString("Status");
+        int period = rs.getInt("Period");
+        float maxPrice = rs.getFloat("MaxPrice");
+        float returnPrice = rs.getFloat("ReturnedPrice");
+        String receiveDate = rs.getString("ReceivedDate");
+
+        ConsignmentDTO consignment = new ConsignmentDTO();
+        consignment.setConsigmentID(consignmentID);
+        consignment.setProductID(productID);
+        if (memberID != null) {
+            consignment.setMemberID(memberID);
+        }
+        consignment.setStoreOwnerID(storeOwnerID);
+        consignment.setName(fullName);
+        consignment.setAddress(address);
+        consignment.setPhone(phone);
+        consignment.setEmail(email);
+        consignment.setCardNumber(cardNumber);
+        consignment.setCardOwner(cardOwner);
+        consignment.setFromDate(fromDate);
+        consignment.setToDate(toDate);
+        consignment.setRaiseWebDate(raiseWebDate);
+        consignment.setStatus(status);
+        consignment.setPeriod(period);
+        consignment.setMaxPrice(maxPrice);
+        consignment.setReturnPrice(returnPrice);
+        consignment.setReceivedDate(receiveDate);
+        
+        return consignment;
     }
 }
