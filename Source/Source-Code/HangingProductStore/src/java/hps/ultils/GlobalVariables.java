f89a79c51b068bb6e64df28821cc809ae2aae783
HoangNH17193 <HoangNHSE61007@fpt.edu.vn>
2015-06-11 14:44:34 +0700

    add account variable

--- Source/Source-Code/HangingProductStore/src/java/hps/ultils/GlobalVariables.java
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/GlobalVariables.java
@@ -15,7 +15,13 @@ public class GlobalVariables {
     public static final String AWS_SECRET_KEY = "J3qeXy1QQHJlwCIX4CpXCeuWP3vN6rs1CpSezubJ";// secret key from amazon, pair with access key
     public static final String ENDPOINT = "ecs.amazonaws.com";// location
     public static final String ASS_TAG = "danqt-20";// account given by amazon so we can send request.
-    
+    //role in account
+    public static final String MEMBER = "member";
+    public static final String STORE_OWNER = "storeOwner";
+    //status in account
+    public static final String ACTIVE = "active";
+    public static final String DEACTIVE = "deactive";
+    //
     public static final String MANAGERMENT_PAGE = "manageProductStatusPage.jsp";
     public static final String PAYMENT_PAGE = "paymentPage.jsp";
     public static final String CANCEL_PAGE = "cancelProductPage.jsp";
@@ -23,10 +29,10 @@ public class GlobalVariables {
     public static final String SESSION_TIME_OUT_PAGE = "errorPage.jsp";
     public static final String SUCCESS_ACTION_PAGE = "successPage.jsp";
     public static final String PUBLISH_PAGE = "publishOnWebPage.jsp";
-    
+
     public static final String SEARCH_PRODUCT_STATUS_SERVLET = "SearchProductStatusServlet";
     public static final String CANCEL_PRODUCT_SERVLET = "CancelProductServlet";
     public static final String MANAGERMENT_SERVLET = "LoadManageProductPageServlet";
     public static final String LOAD_PAYMENT_SERVLET = "LoadPaymentPageServlet";
-    
+
 }
