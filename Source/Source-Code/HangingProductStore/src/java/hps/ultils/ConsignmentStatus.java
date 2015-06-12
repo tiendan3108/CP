bf4cbfdb7f5222b76ca329d33909aa1e2fb24d17
HoangNH17193 <HoangNHSE61007@fpt.edu.vn>
2015-06-12 23:12:35 +0700

    add status for product and Consignment

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/ConsignmentStatus.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.ultils;
+
+/**
+ *
+ * @author HoangNHSE61007
+ */
+public class ConsignmentStatus {
+
+    public static final int WAITING = 1;
+    public static final int REFUSE = 2;
+    public static final int ACCEPTED = 3;
+    public static final int COMPLETED = 4;
+    public static final int RECEIVED = 5;
+}
---
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/ProductStatus.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.ultils;
+
+/**
+ *
+ * @author HoangNHSE61007
+ */
+public class ProductStatus {
+
+    public static final int NOT_AVAILABLE = 1;
+    public static final int AVAILABLE = 2;
+    public static final int ON_WEB = 3;
+    public static final int ORDERED = 4;
+    public static final int SOLD = 5;
+    public static final int CANCEL = 6;
+    public static final int COMPLETED = 7;
+}
