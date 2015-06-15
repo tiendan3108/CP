99815b5a425f83e912f2aea440015e2e6dfd6bd5
HoangNH17193 <HoangNHSE61007@fpt.edu.vn>
2015-06-15 21:04:56 +0700

    add Season

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/Season.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.ultils;
+
+/**
+ *
+ * @author HoangNHSE61007
+ */
+public class Season {
+
+    public static final int spring = 1;
+    public static final int summer = 2;
+    public static final int fall = 3;
+    public static final int winter = 4;
+}
