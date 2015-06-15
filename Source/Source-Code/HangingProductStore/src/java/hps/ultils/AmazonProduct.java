8b68a29389942dc6a46c41f48ba885a3e5f7ac3a
tiendan3108 <tiendan3108@users.noreply.github.com>
2015-06-15 20:03:24 +0700

    update return value of price suggestion

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/AmazonProduct.java
@@ -0,0 +1,107 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package test1;
+
+/**
+ *
+ * @author Tien Dan
+ */
+public class AmazonProduct {
+
+    private String name;
+    private float price;
+    private String ASIN;
+    private String url;
+    private String currency;
+
+    public AmazonProduct(String name, float price, String ASIN, String url, String currency) {
+        this.name = name;
+        this.price = price;
+        this.ASIN = ASIN;
+        this.url = url;
+        this.currency = currency;
+    }
+
+    public AmazonProduct(float price, String ASIN, String currency) {
+        this.price = price;
+        this.ASIN = ASIN;
+        this.currency = currency;
+    }
+
+    public AmazonProduct() {
+    }
+
+    /**
+     * @return the name
+     */
+    public String getName() {
+        return name;
+    }
+
+    /**
+     * @param name the name to set
+     */
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return the price
+     */
+    public float getPrice() {
+        return price;
+    }
+
+    /**
+     * @param price the price to set
+     */
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    /**
+     * @return the ASIN
+     */
+    public String getASIN() {
+        return ASIN;
+    }
+
+    /**
+     * @param ASIN the ASIN to set
+     */
+    public void setASIN(String ASIN) {
+        this.ASIN = ASIN;
+    }
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    /**
+     * @return the currency
+     */
+    public String getCurrency() {
+        return currency;
+    }
+
+    /**
+     * @param currency the currency to set
+     */
+    public void setCurrency(String currency) {
+        this.currency = currency;
+    }
+
+}
--- Source/Source-Code/HangingProductStore/src/java/hps/ultils/AmazonService.java
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/AmazonService.java
@@ -3,80 +3,155 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package hps.ultils;
+package test1;
 
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
 
 /**
  *
  * @author Tien Dan
  */
 public class AmazonService {
-    // ham getprice này truyền 3 tham số
-    // keywords là tên của sản phẩm, brand là hãng
-    // type là loại sản phẩm nằm trong danh sách sau:
-    // Watches,Fashion,Shoes,FashionBoys,FashionGirls,FashionWomen,FashionMen,Jewelry,FashionBaby
-    // hàm trả về -1 nếu ko tìm thấy sản phẩm
-    // trả về số là giá thấp nhất của sản phầm mới theo đơn vị USD
-    public float getPrice(String keywords, String brand, String type) {
-        
+
+    public static final String AWS_ACCESS_KEY_ID = "AKIAJQUQ7BS6CDFYFMLA";// access key id from amazon
+    public static final String AWS_SECRET_KEY = "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";// secret key from amazon, pair with access key
+    public static final String ENDPOINT = "ecs.amazonaws.com";// location
+    public static final String ASS_TAG = "danqt-20";// account given by amazon so we can send request.
+
+    public static void main(String[] args) {
+        AmazonService test = new AmazonService();
+        test.getProduct("Casio MT-G", "Casio", "Watches");
+    }
+
+    public List<AmazonProduct> getProduct(String keywords, String brand, String type) {
+
         SignedRequestsHelper helper;
         try {
-            helper = SignedRequestsHelper.getInstance(GlobalVariables.ENDPOINT, GlobalVariables.AWS_ACCESS_KEY_ID, GlobalVariables.AWS_SECRET_KEY,GlobalVariables.ASS_TAG);
-        } catch (Exception e) {
-            e.printStackTrace();
-            return -1;
+            helper = SignedRequestsHelper.getInstance(ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_KEY, ASS_TAG);
+        } catch (IllegalArgumentException | UnsupportedEncodingException | NoSuchAlgorithmException | InvalidKeyException e) {
+            return null;
         }
-        
-        String requestUrl = null;
-        float price;
 
-        Map<String, String> params = new HashMap<String, String>();
+        String requestUrl = null;
+        List<AmazonProduct> result = new ArrayList<>();
+
+        Map<String, String> params = new HashMap<>();
         params.put("Service", "AWSECommerceService");//ok
         params.put("Operation", "ItemSearch");
-        params.put("Keywords",keywords);
+        params.put("Keywords", keywords);
         params.put("ResponseGroup", "Offers");
         params.put("Condition", "New");
-        params.put("MerchantId","All");
+        params.put("MerchantId", "All");
         params.put("SearchIndex", type);
         params.put("Brand", brand);
 
         requestUrl = helper.sign(params);
-        price = fetchPrice(requestUrl);
         System.out.println(requestUrl);
-        return price;
+        result = fetchProduct(requestUrl);
+        return result;
     }
 
-    private float fetchPrice(String requestUrl) {
-        float price;
+    private List<AmazonProduct> fetchProduct(String requestUrl) {
+        List<AmazonProduct> temp_result = new ArrayList<>();
+        List<AmazonProduct> result = new ArrayList<>();
+        AmazonProduct product = null;
         try {
             DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
             DocumentBuilder db = dbf.newDocumentBuilder();
             Document doc = db.parse(requestUrl);
-            Node titleNode = doc.getElementsByTagName("Amount").item(0);
-            price = Float.parseFloat(titleNode.getTextContent())/100;
+            doc.getDocumentElement().normalize();
+            NodeList source, items, itemContent, offerSummaryContent, lowestNewPriceContent;
+            Node nNode, item, ASINNode, offerSummaryNode, lowestNewPriceNode, priceNode, currencyNode;
+            String ASIN, Currency;
+            Float price;
+            source = doc.getElementsByTagName("Items");
+            nNode = source.item(0);
+            items = nNode.getChildNodes();
+            //start first node
+            for (int i = 4; i < 9; i++) {
+                item = items.item(i);
+                itemContent = item.getChildNodes();
+                ASINNode = itemContent.item(0);
+                ASIN = ASINNode.getTextContent();
+                offerSummaryNode = itemContent.item(1);
+                offerSummaryContent = offerSummaryNode.getChildNodes();
+                lowestNewPriceNode = offerSummaryContent.item(0);
+                lowestNewPriceContent = lowestNewPriceNode.getChildNodes();
+                priceNode = lowestNewPriceContent.item(0);
+                currencyNode = lowestNewPriceContent.item(1);
+                price = Float.parseFloat(priceNode.getTextContent()) / 100;
+                Currency = currencyNode.getTextContent();
+                temp_result.add(new AmazonProduct(price, ASIN, Currency));
+            }
+            for (AmazonProduct temp_product : temp_result) {
+                product = getProductDetail(temp_product);
+                if (product != null) {
+                    result.add(product);
+                }
+            }
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            Logger.getLogger(AmazonService.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return result;
+    }
+
+    private AmazonProduct getProductDetail(AmazonProduct product) {
+        SignedRequestsHelper helper;
+        try {
+            helper = SignedRequestsHelper.getInstance(ENDPOINT, AWS_ACCESS_KEY_ID, AWS_SECRET_KEY, ASS_TAG);
         } catch (Exception e) {
             e.printStackTrace();
-            return -1;
+            return null;
         }
-        return price;
+
+        String requestUrl = null;
+        List<AmazonProduct> result = new ArrayList<AmazonProduct>();
+
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("Service", "AWSECommerceService");//ok
+        params.put("Operation", "ItemLookup");
+        params.put("ResponseGroup", "Large");
+        params.put("Condition", "New");
+        params.put("ItemId", product.getASIN());
+        params.put("MerchantId", "All");
+
+        requestUrl = helper.sign(params);
+        return fetchProductDetail(product, requestUrl);
     }
-    
-    public static void main(String[] args) {
-        AmazonService amazon = new AmazonService();
-        String keyword = "Native Miller";
-        String brand = "";
-        String category = "Shoes";
-        
-        float price = amazon.getPrice(keyword, brand, category);
-        System.out.println("Keyword : " + keyword);
-        System.out.println("Brand   : " + brand);
-        System.out.println("Category: " + category);
-        System.out.println("Price   : " + price);
+
+    private AmazonProduct fetchProductDetail(AmazonProduct product, String requestUrl) {
+        try {
+            DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+            DocumentBuilder db = dbf.newDocumentBuilder();
+            Document doc = db.parse(requestUrl);
+            doc.getDocumentElement().normalize();
+            NodeList DetailPageURLContent = doc.getElementsByTagName("DetailPageURL");
+            Node DetailPageURL = DetailPageURLContent.item(0);
+            product.setUrl(DetailPageURL.getTextContent());
+            NodeList TitleContent = doc.getElementsByTagName("Title");
+            Node Title = TitleContent.item(0);
+            product.setName(Title.getTextContent());
+        } catch (ParserConfigurationException | SAXException | IOException ex) {
+            Logger.getLogger(AmazonService.class.getName()).log(Level.SEVERE, null, ex);
+            return null;
+        }
+        return product;
     }
 }
