55e0cb514c2d2113c4738fa95c3af9be46506900
HoangNH17193 <HoangNHSE61007@fpt.edu.vn>
2015-06-19 15:41:29 +0700

    add MessageString

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/MessageString.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.ultils;
+
+/**
+ *
+ * @author HoangNHSE61007
+ */
+public class MessageString {
+    public static final String orderFail = "Đặt hàng không thành công. Món hàng của bạn đã được người khác đặt trước, xin vui lòng đặt món hàng khác.";
+}
