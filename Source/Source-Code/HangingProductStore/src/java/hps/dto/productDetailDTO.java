d78c49ed7c736ee21bcd1f25a4616453bb338e8f
HoangNH17193 <HoangNHSE61007@fpt.edu.vn>
2015-07-08 09:46:21 +0700

    add productDetailDTO

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/dto/productDetailDTO.java
@@ -0,0 +1,134 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.dto;
+
+/**
+ *
+ * @author HoangNHSE61007
+ */
+public class productDetailDTO {
+    private int productID;
+    private String brand;
+    private String productName;
+    private String address;
+    private String fullName;
+    private String phone;
+    private String email;
+
+    public productDetailDTO() {
+    }
+
+    public productDetailDTO(int productID, String brand, String productName, String address, String fullName, String phone, String email) {
+        this.productID = productID;
+        this.brand = brand;
+        this.productName = productName;
+        this.address = address;
+        this.fullName = fullName;
+        this.phone = phone;
+        this.email = email;
+    }
+    
+    /**
+     * @return the productID
+     */
+    public int getProductID() {
+        return productID;
+    }
+
+    /**
+     * @param productID the productID to set
+     */
+    public void setProductID(int productID) {
+        this.productID = productID;
+    }
+
+    /**
+     * @return the brand
+     */
+    public String getBrand() {
+        return brand;
+    }
+
+    /**
+     * @param brand the brand to set
+     */
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    /**
+     * @return the productName
+     */
+    public String getProductName() {
+        return productName;
+    }
+
+    /**
+     * @param productName the productName to set
+     */
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    /**
+     * @return the address
+     */
+    public String getAddress() {
+        return address;
+    }
+
+    /**
+     * @param address the address to set
+     */
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    /**
+     * @return the fullName
+     */
+    public String getFullName() {
+        return fullName;
+    }
+
+    /**
+     * @param fullName the fullName to set
+     */
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    /**
+     * @return the phone
+     */
+    public String getPhone() {
+        return phone;
+    }
+
+    /**
+     * @param phone the phone to set
+     */
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    /**
+     * @return the email
+     */
+    public String getEmail() {
+        return email;
+    }
+
+    /**
+     * @param email the email to set
+     */
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    
+
+}
