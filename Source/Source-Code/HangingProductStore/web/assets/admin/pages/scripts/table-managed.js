4ed8094c90257beada681fda55236c165d9e98e6
tiendan3108 <tiendan3108@users.noreply.github.com>
2015-07-19 17:01:45 +0700

    update table-managed.js add consignment table and product table

--- Source/Source-Code/HangingProductStore/web/assets/admin/pages/scripts/table-managed.js
+++ Source/Source-Code/HangingProductStore/web/assets/admin/pages/scripts/table-managed.js
@@ -1261,13 +1261,210 @@ var TableManaged = function () {
         tableWrapper.find('.dataTables_length select').addClass("form-control input-xsmall input-inline"); // modify table per page dropdown
     }
 
+    var initConsignmentTable = function () {
+
+        var table = $('#consignmentTable');
+
+        // begin first table
+        table.dataTable({
+            // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+            "language": {
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                },
+                "emptyTable": "Không có dữ liệu",
+                "info": "Hiện _START_ đến _END_ của _TOTAL_ dữ liệu",
+                "infoEmpty": "Không tìm thấy yêu cầu phù hợp",
+                "infoFiltered": "(filtered1 from _MAX_ total entries)",
+                "lengthMenu": "Hiện _MENU_ dữ liệu",
+                "search": "Tìm kiếm:",
+                "zeroRecords": "Không tìm thấy yêu cầu phù hợp"
+            },
+            // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+            // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+            // So when dropdowns used the scrollable div should be removed. 
+            //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+            "bStateSave": false, // save datatable state(pagination, sort, etc) in cookie.
+
+            "columns": [{
+                    "orderable": false
+                }, {
+                    "orderable": true
+                }, {
+                    "orderable": false
+                }, {
+                    "orderable": false
+                }, {
+                    "orderable": false
+                }, {
+                    "orderable": true
+                }],
+            "lengthMenu": [
+                [5, 10, 20, -1],
+                [5, 10, 20, "All"] // change per page values here
+            ],
+            // set the initial value
+            "pageLength": 5,
+            "pagingType": "bootstrap_full_number",
+            "language": {
+                "search": "Tìm: ",
+                "lengthMenu": "  _MENU_ yêu cầu",
+                "paginate": {
+                    "previous": "Prev",
+                    "next": "Next",
+                    "last": "Last",
+                    "first": "First"
+                }
+            },
+            "columnDefs": [{// set default column settings
+                    'orderable': false,
+                    'targets': [0]
+                }, {
+                    "searchable": false,
+                    "targets": [0]
+                }],
+            "order": [
+                [0, "asc"]
+            ] // set first column as a default sort by asc
+        });
+
+        var tableWrapper = jQuery('#sample_1_wrapper');
+
+        table.find('.group-checkable').change(function () {
+            var set = jQuery(this).attr("data-set");
+            var checked = jQuery(this).is(":checked");
+            jQuery(set).each(function () {
+                if (checked) {
+                    $(this).attr("checked", true);
+                    $(this).parents('tr').addClass("active");
+                } else {
+                    $(this).attr("checked", false);
+                    $(this).parents('tr').removeClass("active");
+                }
+            });
+            jQuery.uniform.update(set);
+        });
+
+        table.on('change', 'tbody tr .checkboxes', function () {
+            $(this).parents('tr').toggleClass("active");
+        });
+
+        tableWrapper.find('.dataTables_length select').addClass("form-control input-xsmall input-inline"); // modify table per page dropdown
+
+    }
+
+    var initProductTable = function () {
+
+        var table = $('#productTable');
+        var startDate = $('#fromDateProduct').val();
+        var endDate = $('#toDateProduct').val();
+
+        // begin first table
+        table.dataTable({
+            // Internationalisation. For more info refer to http://datatables.net/manual/i18n
+            "language": {
+                "aria": {
+                    "sortAscending": ": activate to sort column ascending",
+                    "sortDescending": ": activate to sort column descending"
+                },
+                "emptyTable": "Không có dữ liệu",
+                "info": "Hiện _START_ đến _END_ của _TOTAL_ dữ liệu",
+                "infoEmpty": "Không tìm thấy sản phẩm phù hợp",
+                "infoFiltered": "(filtered1 from _MAX_ total entries)",
+                "lengthMenu": "Hiện _MENU_ dữ liệu",
+                "search": "Tìm kiếm:",
+                "zeroRecords": "Không tìm thấy sản phẩm phù hợp"
+            },
+            // Uncomment below line("dom" parameter) to fix the dropdown overflow issue in the datatable cells. The default datatable layout
+            // setup uses scrollable div(table-scrollable) with overflow:auto to enable vertical scroll(see: assets/global/plugins/datatables/plugins/bootstrap/dataTables.bootstrap.js). 
+            // So when dropdowns used the scrollable div should be removed. 
+            //"dom": "<'row'<'col-md-6 col-sm-12'l><'col-md-6 col-sm-12'f>r>t<'row'<'col-md-5 col-sm-12'i><'col-md-7 col-sm-12'p>>",
+
+            "bStateSave": false, // save datatable state(pagination, sort, etc) in cookie.
+
+            "columns": [{
+                    "orderable": false
+                }, {
+                    "orderable": true
+                }, {
+                    "orderable": true
+                }, {
+                    "orderable": true
+                }, {
+                    "orderable": false
+                }, {
+                    "orderable": false
+                }, {
+                    "orderable": false
+                }, {
+                    "orderable": true
+                }, {
+                    "orderable": true
+                }],
+            "lengthMenu": [
+                [5, 10, 20, -1],
+                [5, 10, 20, "All"] // change per page values here
+            ],
+            // set the initial value
+            "pageLength": 5,
+            "pagingType": "bootstrap_full_number",
+            "language": {
+                "search": "Tìm: ",
+                "lengthMenu": "  _MENU_ sản phẩm",
+                "paginate": {
+                    "previous": "Prev",
+                    "next": "Next",
+                    "last": "Last",
+                    "first": "First"
+                }
+            },
+            "columnDefs": [{// set default column settings
+                    'orderable': false,
+                    'targets': [0]
+                }, {
+                    "searchable": false,
+                    "targets": [0]
+                }],
+            "order": [
+                [0, "asc"]
+            ] // set first column as a default sort by asc
+        });
+
+        var tableWrapper = jQuery('#sample_1_wrapper');
+
+        table.find('.group-checkable').change(function () {
+            var set = jQuery(this).attr("data-set");
+            var checked = jQuery(this).is(":checked");
+            jQuery(set).each(function () {
+                if (checked) {
+                    $(this).attr("checked", true);
+                    $(this).parents('tr').addClass("active");
+                } else {
+                    $(this).attr("checked", false);
+                    $(this).parents('tr').removeClass("active");
+                }
+            });
+            jQuery.uniform.update(set);
+        });
+
+        table.on('change', 'tbody tr .checkboxes', function () {
+            $(this).parents('tr').toggleClass("active");
+        });
+
+        tableWrapper.find('.dataTables_length select').addClass("form-control input-xsmall input-inline"); // modify table per page dropdown
+
+    }
+
     return {
         //main function to initiate the module
         init: function () {
             if (!jQuery().dataTable) {
                 return;
             }
-
+            initProductTable();
+            initConsignmentTable();
             initSoldTable();
             initCompletedTable();
             initCanceledTable();
