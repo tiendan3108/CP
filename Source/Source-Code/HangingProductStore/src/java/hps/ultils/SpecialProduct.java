0a0aa1106dff3fd06f053fbb55bfc8ec95204a6a
HoangNH17193 <HoangNHSE61007@fpt.edu.vn>
2015-07-23 18:43:53 +0700

    add SpecialProduct

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/SpecialProduct.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.ultils;
+
+/**
+ *
+ * @author HoangNHSE61007
+ */
+public class SpecialProduct {
+    public static final int SPECIAL = 1;
+    public static final int NOT_SPECIAL = 0;
+}
