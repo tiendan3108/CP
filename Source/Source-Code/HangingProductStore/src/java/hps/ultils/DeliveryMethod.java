bffb41593661f849b4168af6e737acb774f996c8
HoangNH17193 <HoangNHSE61007@fpt.edu.vn>
2015-07-31 09:08:56 +0700

    add DeliveryMethod

---
+++ Source/Source-Code/HangingProductStore/src/java/hps/ultils/DeliveryMethod.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hps.ultils;
+
+/**
+ *
+ * @author HoangNHSE61007
+ */
+public class DeliveryMethod {
+
+    public static int BY_CUSTOMER = 1;
+    public static int BY_STOREOWNER = 0;
+}
